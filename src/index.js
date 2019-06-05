21ee673f3b38661baf345463d056c8d286c811cf
marjo1994 <m_rhl@live.com>
2019-06-05 12:00:29 -0500

    Test con jest/ Try

--- src/index.js
+++ src/index.js
@@ -1,6 +1,25 @@
 
 import path from 'path'
+import fs from 'fs' 
 
 export const isAbsolute = file => {
-    path.isAbsolute(file);
+   return path.isAbsolute(file);
 };
+
+export const convertToAbsolute = file =>{
+    return path.resolve(file);
+};
+
+export const isFile = file => {
+    const stats = fs.statSync(file);
+   return stats.isFile();
+}
+
+export const  extensionName = file => {
+   return path.extname(file);
+}
+
+
+/* fs.stat(file,callback(err,stats))
+stats.isFile()
+stats.isDirectory()*/
