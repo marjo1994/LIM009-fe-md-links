2284c9fb02cd47155503a780c06a6993051de53e
marjo1994 <m_rhl@live.com>
2019-06-05 19:08:29 -0500

    Test

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 
 import path from 'path'
-import fs from 'fs' 
+import fs from 'fs'
+const fspromises = fs.promises
 
 export const isAbsolute = file => {
    return path.isAbsolute(file);
@@ -10,10 +11,13 @@ export const convertToAbsolute = file =>{
     return path.resolve(file);
 };
 
-export const isFile = file => {
+
+// fsPromises.stat(path[, options])
+
+/* export const isFile = file => {
     const stats = fs.statSync(file);
    return stats.isFile();
-}
+} */
 
 export const  extensionName = file => {
    return path.extname(file);
--- test/index.spec.js
+++ test/index.spec.js
@@ -1,12 +1,26 @@
-import { isAbsolute } from "../src/index.js";
+import { isAbsolute, convertToAbsolute } from "../src/index.js";
 
 describe('Es una función que permite conocer si la ruta es absoluta o no',() => {
     it('isAbsolute debería ser una función', () => {
         expect(typeof isAbsolute).toBe('function');
     });
     it('Es una ruta absoluta', () => {
-        expect(isAbsolute('../example/example_relative.js')).toBe(false)        
+        expect(isAbsolute('../LIM009-fe-md-links/example/example_relative.js')).toBe(false)        
       });
+    it('Es una ruta absoluta', () => {
+        expect(isAbsolute('/home/marjorie/Documentos/md-links/LIM009-fe-md-links/example/example_relative.js')).toBe(true)        
+    });
 });
+
+describe('Es una función que permite convertir una ruta en absoluta',() => {
+    it('isAbsolute debería ser una función', () => {
+        expect(typeof convertToAbsolute).toBe('function');
+    });
+    it('Convierte una ruta relativa en ruta absoluta', () => {
+        expect(convertToAbsolute('../LIM009-fe-md-links/example/example_relative.js')).toEqual('/home/marjorie/Documentos/md-links/LIM009-fe-md-links/example/example_relative.js')     
+      });     
+});
+
+
 
 
