12f71c17e7e95e04ca9b49aea88fd3005b3eb325
marjo1994 <m_rhl@live.com>
2019-06-07 18:02:23 -0500

    Try function to get paths of a directory

---
+++ example/example.md
@@ -0,0 +1,1 @@
+Este es otro.  Cras porta magna at sapien mollis pharetra.  Duishendrerit scelerisque lectus, eget pellentesque erat volutpat vel.Nulla eget viverra sem, sed pharetra orci.  Etiam a iaculis leo.  Utviverra eleifend tortor.
--- src/index.js
+++ src/index.js
@@ -12,12 +12,33 @@ export const convertToAbsolute = file => {
     return path.resolve(file);
 };
                                                                      
-export const verifyIsFile = file => { // verificar si la ruta es archivo
+export const verifyIsFile = file => { 
+   /// verificar si la ruta es archivo
    return fspromises.stat(file) // retorna promesa con un objeto
    .then (result => {
-      console.log(result.isFile()); // retorna el booleano con el valor 
+      return result.isFile(); // retorna el booleano con el valor 
    })
 };
+
+export const  extensionName = file => {
+   return path.extname(file);
+}
+// extensionName('/home/marjorie/Documentos/md-links/LIM009-fe-md-links/example/example.md');
+
+/* Verificar si es un archivo.md */
+const pathAbsolute = convertToAbsolute('../LIM009-fe-md-links/example/example_relative.js');
+
+verifyIsFile(pathAbsolute).then(result => {
+  if(result === true) {
+      if(extensionName(pathAbsolute) === '.md') {
+         readfile(pathAbsolute).then(console.log)
+      } else {
+         console.log('no es un archivo .md')
+      }
+   } else {
+      console.log('es un directorio')
+   };
+});
 
 export const verifyDirectory = file => {
    return fspromises.stat(file)
@@ -35,17 +56,42 @@ export const readDirectory = file => {
        return result;
       // console.log(result)
     })
-    .catch(console.log('error'))
 };
-readDirectory('/home/marjorie/Documentos/md-links/LIM009-fe-md-links/example')
-.then(r => r.map(convertToAbsolute))
-.then(p => console.log(p));
 
-export const  extensionName = file => {
-   console.log(path.extname(file));
+const getPathsMarkdowns = (pathDir) => {
+  const arrPaths = [];
+  readDirectory(pathDir)
+   .then(r => r.map((e) => {
+      return path.join(pathDir,e);
+   }))
+   .then(p => {
+      p.forEach(pathAbsolute => {
+         verifyIsFile(pathAbsolute).then(result => {
+            if(result === true) {
+               if(extensionName(pathAbsolute) === '.md') {
+                  return arrPaths.push(pathAbsolute);
+                  // readfile(pathAbsolute).then(console.log)
+               } else {
+                  console.log('no es un archivo .md')
+               }
+            } else {
+               console.log('es un directorio')
+            };
+         });
+      });
+   });
+   return arrPaths
 }
 
-// extensionName('/home/marjorie/Documentos/md-links/LIM009-fe-md-links/example/example.md');
+getPathsMarkdowns('/home/marjorie/Documentos/md-links/LIM009-fe-md-links/example').then(result => console.log(result));
+
+   
+export const readFile = file => {
+   return fspromises.readFile(file)
+   .then(buffer => buffer.toString());
+}
+
+// readFile('/home/marjorie/Documentos/md-links/LIM009-fe-md-links/example/example.md');
 
 // Funciones compuestas 
 // const throughDirectory = (file, callback) => {
