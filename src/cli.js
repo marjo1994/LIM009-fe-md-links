21fde873ff62adcef16bde362b8e0477658cd2b1
marjo1994 <m_rhl@live.com>
2019-06-21 20:40:09 -0500

    cambios en ruta link

--- src/cli.js
+++ src/cli.js
@@ -6,19 +6,13 @@ const [,, ...args] = process.argv;
 // Print hello world provided args.
 // console.log(args)
 
-import {mdLinksCli} from "./md-links-cli.js"
+import {mdLinksCli} from './md-links-cli.js';
 
 const path = args[0];
 const arrOptions = args.slice(1);
 
 mdLinksCli(path, arrOptions)
-.then(result => console.log(result))
-.catch(error => console.log(error.message))
-
-
-
-
-
-
+  .then(result => console.log(result))
+  .catch(error => console.log(error.message));
          
 
